b7e70fbabb9b5263dea13fe5ef47065227192e9d
PoprostuTaki <rafpiekar@gmail.com>
2024-11-05 17:39:14 +0100

    Project initialization

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <main className="space-y-8">
+      <h1>Hello WOrld</h1>
+      <Button>Click me.</Button>
+    </main>
+  );
+}
+
+export default App;
